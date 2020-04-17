feadbf0485cf6833a620705c354b025f061916d9
Jessica Gamez <jpgamez1789@icloud.com>
2020-04-17 02:25:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,9 @@
 def introduction(name)
-  puts "hi, my name is #{name}."
-end
+  puts "Hi, my name is #{name}."
+end 
+
+def introduction_with_language{name, language}
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+
+def introduction_with_language("Josh, Ruby")
